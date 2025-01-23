572c7c31554fb91eaf778489f6f80f56563eb2ff
h581876 <581876@stud.hvl.no>
2025-01-23 10:29:53 +0100

    Main og grensesnitt

---
+++ src/no/hvl/data102/filmarkiv/klient/FilmarkivMain.java
@@ -0,0 +1,11 @@
+package no.hvl.data102.filmarkiv.klient;
+
+public class FilmarkivMain {
+
+	public static void main(String[] args) {
+		FilmarkivADT filma = new Filmarkiv(100);
+		Meny meny = new Meny(filma);
+		meny.start();
+	}
+
+}
